3d3518a4c9f58a57e833fdd18453213841b8a2af
welshfruit <meganclairewillis@gmail.com>
2014-04-22 10:29:44 +0100

    Responsive break points,social media&layout amends

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,5 @@
+//@prepros-prepend vendor/enquire.js
+
 $(document).ready(function(){
 	var $body = $("body"),
 		$burgerNav = $("#burger-nav");
@@ -7,4 +9,71 @@ $(document).ready(function(){
 		$body.toggleClass("burger-nav-active");
 	});
 
+});
+
+//Phablet - 38.75em
+var phablet = "38.75em";
+
+//Desklet - 55.625em
+var desklet = "55.625em";
+
+enquire.register("screen and (min-width:" + phablet + ")", {
+
+    // OPTIONAL
+    // If supplied, triggered when a media query matches.
+    match : function() {
+
+    	//alert("Phab!");
+    },      
+                                
+    // OPTIONAL
+    // If supplied, triggered when the media query transitions 
+    // *from a matched state to an unmatched state*.
+    unmatch : function() {},    
+    
+    // OPTIONAL
+    // If supplied, triggered once, when the handler is registered.
+    setup : function() {
+    	//alert("Phab setup");
+    },    
+                                
+    // OPTIONAL, defaults to false
+    // If set to true, defers execution of the setup function 
+    // until the first time the media query is matched
+    deferSetup : true,
+                                
+    // OPTIONAL
+    // If supplied, triggered when handler is unregistered. 
+    // Place cleanup code here
+    destroy : function() {}
+      
+});
+
+enquire.register("screen and (min-width:" + desklet + ")", {
+
+    // OPTIONAL
+    // If supplied, triggered when a media query matches.
+    match : function() {
+    	//alert("Desk!");
+    },      
+                                
+    // OPTIONAL
+    // If supplied, triggered when the media query transitions 
+    // *from a matched state to an unmatched state*.
+    unmatch : function() {},    
+    
+    // OPTIONAL
+    // If supplied, triggered once, when the handler is registered.
+    setup : function() {},    
+                                
+    // OPTIONAL, defaults to false
+    // If set to true, defers execution of the setup function 
+    // until the first time the media query is matched
+    deferSetup : true,
+                                
+    // OPTIONAL
+    // If supplied, triggered when handler is unregistered. 
+    // Place cleanup code here
+    destroy : function() {}
+      
 });
